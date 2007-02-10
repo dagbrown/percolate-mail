f7a9e68eb4ae6dac1359e820006f584f85308b48
dagbrown <dagbrown>
2007-02-10 23:27:27 +0000

    r921@aerogel:  dagbrown | 2007-02-11 08:26:01 +0900  Removed the extraneous gurgitate bits, changed other gurgitate bits to  percolate bits.

--- install.rb
+++ install.rb
@@ -3,8 +3,8 @@
 require "rbconfig"
 require "ftools"
 
-module Gurgitate
-    Package = "gurgitate-mail"
+module Percolate
+    Package = "percolate-mail"
 
     class Install
         def self.mkdir(d)
@@ -29,58 +29,28 @@ module Gurgitate
             include Config
 
             if prefix then
-                bindir = File.join prefix, "bin"; mkdir bindir
-                dest   = File.join prefix, "lib"; mkdir dest
-                mkdir File.join(prefix, "man")
-                mandir = File.join prefix, "man", "man1"; mkdir mandir
+                dest   = File.join prefix, "lib"
+                mkdir dest
             else
                 version = CONFIG["MAJOR"] + "." + CONFIG["MINOR"]
                 sitedir = CONFIG["sitedir"]
-                bindir  = CONFIG["bindir"]
-                mandir  = File.join(CONFIG["mandir"],"man1")
                 dest    = File.join(sitedir,version)
             end
 
-            destgur = File.join(dest,"gurgitate")
-            destdel = File.join(destgur,"deliver")
+            destper = File.join(dest,"percolate")
 
             print "Installing #{Package}.rb in #{dest}...\n"
-            File.install("#{Package}.rb", dest, 0644)
+            File.install(File.join("lib","#{Package}.rb"), dest, 0644)
             
-            mkdir destgur
-            Dir.glob(File.join("gurgitate","*.rb")).each { |f|
-                puts "Installing #{f} in #{destgur}..."
-                File.install(f,destgur)
+            mkdir destper
+            Dir.glob(File.join("lib","percolate","*.rb")).each { |f|
+                puts "Installing #{f} in #{destper}..."
+                File.install(f,destper)
             }
-
-            mkdir destdel
-
-            Dir.glob(File.join(File.join("gurgitate","deliver"),"*.rb")).each {
-            |f|
-                puts "Installing #{f} in #{destdel}..."
-                File.install(f,destdel)
-            }
-
-            print "Installing #{Package}.1 in #{mandir}...\n"
-            File.install("#{Package}.man","#{mandir}/#{Package}.1", 0644)
-
-            print "Installing #{Package} in #{bindir}...\n"
-            # Not so simple; need to put in the shebang line
-            from_f=File.open("gurgitate-mail")
-            to_f=File.open("#{bindir}/gurgitate-mail","w")
-            to_f.print("#!#{bindir}/ruby -w\n\n")
-            from_f.each do |l|
-                to_f.print l
-            end
-
-            to_f.close()
-            from_f.close()
-
-            File.chmod(0755,"#{bindir}/gurgitate-mail")
         end
     end
 end
 
 if __FILE__ == $0 then
-    Gurgitate::Install.install()
+    Percolate::Install.install()
 end
