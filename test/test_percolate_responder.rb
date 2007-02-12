07ea91107950fd3a159e54810f4e3cb69702884f
dagbrown <dagbrown>
2007-02-12 23:03:30 +0000

    r1000@aerogel:  dagbrown | 2007-02-12 21:22:38 +0900  And one more test to bring me back up to 100% code coverage

--- test/test_percolate_responder.rb
+++ test/test_percolate_responder.rb
@@ -351,6 +351,17 @@ class TestSubclassedSMTPResponder < TestPercolateResponder
         assert_match /^250 honk/, @responder.response
     end
         
+    def test_data_doofus_response_message_with_text_only
+        test_rcpt_to_valid
+        @responder.message_validation = "honk"
+        @responder.command "data"
+        assert_equal "354 end data with <cr><lf>.<cr><lf>", 
+            @responder.response
+        @responder.command "Boxcar!"
+        assert_equal nil, @responder.response
+        @responder.command "."
+        assert_match /^250 honk/, @responder.response
+    end
 
     if $LOADED_FEATURES.include? "gurgitate/mailmessage.rb"
 
